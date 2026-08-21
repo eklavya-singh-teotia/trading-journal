1c07e6834f4f2c036d9fd98c18acda00722a9a5d
eklavya-singh-teotia <seklavya592@gmail.com>
2026-08-21 22:56:25 +0530

    debug

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import connectDB from "./db/connectDB.js";
 import app from "./app.js";
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 connectDB()
     .then(() => {
         app.listen(PORT, () => {
